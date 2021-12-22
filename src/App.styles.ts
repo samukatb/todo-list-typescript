734400a9f6c3a46648555edb3505683b7372723c
samuka321 <samukatb2010@hotmail.com>
2021-12-22 10:39:09 -0300

    Created TO-DO list with react/typescrypt

---
+++ src/App.styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 0;
+  padding: 0;
+  background-color: #17181F;
+  color: #797A81;
+  min-height: 100vh;
+`;
+
+export const Area = styled.div`
+  margin: auto;
+  max-width: 980px;
+  padding: 10px;
+`;
+
+export const Header = styled.h1`
+  color: #FFF;
+  text-align: center;
+  border-bottom: 1px solid #444;
+  padding-bottom: 20px;
+`;
+
+export const TextP = styled.p`
+  text-align: center;
+  font-size: 18px
+`;
